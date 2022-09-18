ee3c64a23d8032d76d67879973097b03c4740d43
ferxxoDev <fernando25mendoza95@gmail.com>
2022-09-17 21:16:11 -0400

    update border

--- src/components/QuoteBoxBtn.jsx
+++ src/components/QuoteBoxBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const QuoteBoxBtn = ({getRandom}) => {
   return (
     <div>
-        <i onClick={getRandom} className='bx bxs-chevron-right-circle bx-lg'></i>
+        <i onClick={getRandom} className='bx bxs-chevron-right-circle  '></i>
         
     </div>
   )
